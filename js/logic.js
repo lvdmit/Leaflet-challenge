8e7427a16e090ebde876bd29dd6c12de3c01a179
LarisaD <38594780+lvdmit@users.noreply.github.com>
2018-11-21 23:39:47 -0800

    updating logic.js

--- js/logic.js
+++ js/logic.js
@@ -1,4 +1,3 @@
-// function create (earthquakeCircles) {
 // Create the tile layer that will be the background of our map
 var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}?access_token={accessToken}", {
   attribution: "Map data &copy; <a href=\"http://openstreetmap.org\">OpenStreetMap</a> contributors, <a href=\"http://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"http://mapbox.com\">Mapbox</a>",
